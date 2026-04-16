2218a56dcdc45cffc3d8c9d918ba8a4e7e3f42c1
ViniNovack <vininomn@gmail.com>
2026-04-16 18:40:33 -0300

    Atualizações iniciais das aulas 1, 2, 3 e 4

---
+++ aula 4/aula 4.py
@@ -0,0 +1,28 @@
+# Declaração de Classe
+class Gafanhoto:
+    def __init(self):            # Método Construtor
+        # Atributos de Instancia
+        self.nome = ''
+        self.idade = 0
+    
+    # Metodos de Instância
+    def aniversario(self):
+        self.idade +=1
+    
+    def mensagem(self):
+        return f'{self.nome} é Gafanhoto(a) e tem {self.idade} anos de idade.'
+
+# Declaração de Objetos
+g1 = Gafanhoto()
+g1.nome = 'Maria'
+g1.idade = 17
+print(g1.mensagem())
+
+g2 = Gafanhoto()
+g2.nome = 'Mauro'
+g2.idade = 53
+g2.aniversario()
+print(g2.mensagem())
+
+g3 = Gafanhoto()
+print(g3.mensagem())
---
+++ aula 4/exercicio 1.py
@@ -0,0 +1,20 @@
+class Gafanhoto:
+    def __init(self):
+        self.nome = ''
+        self.idade = 0
+    
+    def aniversario(self):
+        self.idade +=1
+
+    def mensagem(self):
+        return f'{self.nome} têm {self.idade} anos.'
+
+g1 = Gafanhoto()
+g1.nome = 'Carlos'
+g1.idade = 88
+print(g1.mensagem())
+
+g2 = Gafanhoto()
+g2.nome = 'Marina'
+g2.idade = 33
+print(g2.mensagem())
