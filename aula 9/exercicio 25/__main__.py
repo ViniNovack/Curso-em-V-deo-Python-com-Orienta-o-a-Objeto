0109f4e3ac73bf73b7c93660ff6b65cf9fdc57ea
ViniNovack <vininomn@gmail.com>
2026-04-28 21:12:23 -0300

    Atualização

--- aula 9/exercicio 25/__main__.py
+++ aula 9/exercicio 25/__main__.py
@@ -6,17 +6,17 @@ def main():
     distancia = 10
     
     t1 = Moto(distancia)
-    print(f'Frete de {t1.nome()} em {distancia}Km = {t1.calc_frete()}')
+    print(f'Frete de {t1.nome()} em {distancia}Km = R${t1.calc_frete()}')
 
     print('=-'*30)
 
     t2 = Drone(distancia)
-    print(f'Frete de {t2.nome()} em {distancia}km = {t2.calc_frete()}')
+    print(f'Frete de {t2.nome()} em {distancia}km = R${t2.calc_frete()}')
 
     print('=-'*30)
 
     t3 = Caminhao(distancia)
-    print(f'Frete de {t3.nome()} em {distancia}km = {t3.calc_frete()}')
+    print(f'Frete de {t3.nome()} em {distancia}km = R${t3.calc_frete()}')
 
 if __name__ == '__main__':
     main()
