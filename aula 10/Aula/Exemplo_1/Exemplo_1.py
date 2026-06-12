e425a75a5300a62deb91810aee33ce0319808754
ViniNovack <vininomn@gmail.com>
2026-06-12 15:52:40 -0300

    Atualizaçãoes

--- aula 10/Aula/Exemplo_1/Exemplo_1.py
+++ aula 10/Aula/Exemplo_1/Exemplo_1.py
@@ -3,30 +3,35 @@ class ContaBancaria:
     Cria uma conta bancária e permite fazer saques e depósitos
     """
     def __init__(self, id, nome, saldo=0):
-        self.id = id
-        self.titular = nome
-        self.saldo = saldo
-        print(f'Conta {self.id} criada com sucesso. Saldo atual de R${self.saldo:.2f}')
+        self.id = id # Público(+)
+        self._titular = nome # Protegido(#)
+        self.__saldo = saldo # Privado(-)
+        print(f'Conta {self.id} criada com sucesso. __saldo atual de R${self.__saldo:.2f}')
 
     def __str__(self):
-        # return f'A conta {self.id} de {self.titular} tem R${self.saldo:.2f} de saldo.'
+        # return f'A conta {self.id} de {self._titular} tem R${self.__saldo:.2f} de __saldo.'
         return f'Estado atual da conta: {self.__dict__}'             # "__dict__" ele tem a função de mostrar as variaveis com seus respectivos valores da função mâe
     
     def depositar(self, valor):
-        self.saldo +=valor
+        valor = abs(valor)             # O "abs" transforma uma valor no seu valor absoluto(Exemplo: -2 -> 2 / -77 -> 77)
+        self.__saldo +=valor
         print(f'Depósito de R${valor:.2f} autorizado na conta {self.id}')
 
     def sacar(self, valor):
-        if valor > self.saldo:
+        valor = abs(valor)
+        if valor > self.__saldo:
             x = str(input('Você não tem todo esse dinheiro, mas você pode sacar todo o valor da comta? (Y/N)\n'))
             if (x.lower()).strip() == 'y':
-                self.sacar(self.saldo)
-                print(f'Saque de R${self.saldo:.2f} autorizado na conta {self.id}')
+                self.sacar(self.__saldo)
+                print(f'Saque de R${self.__saldo:.2f} autorizado na conta {self.id}')
         else:
-            self.saldo -=valor
+            self.__saldo -=valor
             print(f'Saque de R${valor:.2f} autorizado na conta {self.id}')
 
 """
+    ENCAPSULAMENTO
+    - Visa manter a integridade do sistema, protegendo o estado interno do objeto contra interferência externa não regulamentada
+
     Existem três tipos de visibilidade para atributos em linguagem POO:
     > public +
     > protected #
--- aula 5/Aula 5/part.2/aula 5 part.2.py
+++ aula 5/Aula 5/part.2/aula 5 part.2.py
@@ -4,12 +4,12 @@ class ContaBancaria:
     """
     def __init__(self, id, nome, saldo=0):
         self.id = id
-        self.titular = nome
+        self._titular = nome
         self.saldo = saldo
         print(f'Conta {self.id} criada com sucesso. Saldo atual de R${self.saldo:.2f}')
 
     def __str__(self):
-        return f'A conta {self.id} de {self.titular} tem R${self.saldo:.2f} de saldo.'
+        return f'A conta {self.id} de {self._titular} tem R${self.saldo:.2f} de saldo.'
     
     def depositar(self, valor):
         self.saldo +=valor
