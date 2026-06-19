3d3f9e588bc3600eaa1cbc5eeaf81bf7c46ed4a9
ViniNovack <vininomn@gmail.com>
2026-06-19 18:17:12 -0300

    Desenvolvimento desafio 31

--- aula 12/Desafio 31/__main__.py
+++ aula 12/Desafio 31/__main__.py
@@ -1,7 +1,15 @@
 from retangulo import Retangulo
+from rich import inspect
 
 def main():
-    pass
+    r = Retangulo()
+
+    r.base = 12
+    r.altura = 33
+    print(r.medidas)
+
+    r.medidas = (9, 3)
+    print(r.medidas)
 
 if __name__ == '__main__':
     main()
--- aula 12/Desafio 31/retangulo.py
+++ aula 12/Desafio 31/retangulo.py
@@ -1,2 +1,44 @@
 class Retangulo():
-    pass
+    def __init__(self, base=0, altura=0):
+        self._base = base
+        self._altura = altura
+        self._medidas = (self._base, self._altura)
+        self._area = None
+    
+    @property
+    def base(self):
+        return self._base
+    
+    @base.setter
+    def base(self, valor):
+        if valor <= 0:
+            raise ValueError('Não se pode inseir valores negativos ou 0')
+        else:
+            self._base = valor
+    
+    @property
+    def altura(self):
+        return self._altura
+    
+    @altura.setter
+    def altura(self, valor):
+        if valor <= 0:
+            raise ValueError('Não se pode inserir valores negativos ou 0')
+        else:
+            self._altura = valor
+
+    @property
+    def area(self):
+        return (self._base * self._altura)
+
+    @property
+    def medidas(self):
+        return self._medidas
+
+    @medidas.setter
+    def medidas(self, medidas):
+        if (medidas[0] <= 0) or (medidas[1] <= 0):
+            raise ValueError('Não se pode inserir valores negativos ou 0')
+        else:
+            self._medidas = (medidas[0], medidas[1])
+        return f'Base = {self.base}\nAltura = {self.altura}\nÁrea = {self.area}'
