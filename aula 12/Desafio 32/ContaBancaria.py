abd3ff3dd8f860c23e3c95288d2d3e4bc82d746b
ViniNovack <vininomn@gmail.com>
2026-06-22 18:35:28 -0300

    Atualização

--- aula 12/Desafio 32/ContaBancaria.py
+++ aula 12/Desafio 32/ContaBancaria.py
@@ -1,17 +1,64 @@
+import hashlib
+
 class ContaBancaria:
-    def __init__(self, id, nome, saldo=0):
-        self.id = id
+    def __init__(self, id, nome, senha='', saldo=0):
+        self._id = id
         self._titular = nome
         self.__saldo = saldo
-        print(f'Conta {self.id} criada com sucesso. __saldo atual de R${self.__saldo:.2f}')
+        if senha == '':
+            senha = str(input("Digite uma senha para sua protação: "))
+        self.__senha = hashlib.sha256(senha.encode('utf-8')).hexdigest()
 
-    def __str__(self):
-        return f'Estado atual da conta: {self.__dict__}'
+    def __str__(self, senha=''):
+        if senha == '':
+            senha = str(input("Digite uma senha para sua protação: "))
+            senha = hashlib.sha256(senha.encode('utf-8')).hexdigest()
+        else:
+            senha = hashlib.sha256(senha.encode('utf-8')).hexdigest()
+
+        if senha == self.__senha:
+            return f'Acesso CONCEDIDO, Bem vindo {self._titular}\nEstado atual da conta: {self.__dict__}'
+        else:
+            return 'Acesso NEGADO!'
+
+    @property
+    def saldo(self):
+        return self.__saldo
     
+    @saldo.setter
+    def saldo(self, valor=''):
+        if valor == '':
+            print('Não é permitido trocar o valor da conta, apenas fazer saques e depositos')
+        else:
+            print('Não é permitido trocar o valor da conta, apenas fazer saques e depositos')
+
+    @property
+    def titular(self):
+        return self._titular
+    
+    @titular.setter
+    def saldo(self, nome):
+        if isinstance(nome, int):
+            print('Um nome não pode ter apenas números\nNome novo INVALIDO')
+        else:
+            self._titular = nome
+            print(f'O nome {nome} foi registrado com sucesso!')
+
+    @property
+    def id(self):
+        return self._id
+    
+    @id.setter
+    def id(self, id):
+        if isinstance(id, int):
+            self._id = id
+        else:
+            print('A troca foi invalida, só se podem números no id')
+
     def depositar(self, valor):
         valor = abs(valor)
         self.__saldo +=valor
-        print(f'Depósito de R${valor:.2f} autorizado na conta {self.id}')
+        print(f'Depósito de R${valor:.2f} autorizado na conta {self._id}')
 
     def sacar(self, valor):
         valor = abs(valor)
@@ -19,7 +66,7 @@ class ContaBancaria:
             x = str(input('Você não tem todo esse dinheiro, mas você pode sacar todo o valor da comta? (Y/N)\n'))
             if (x.lower()).strip() == 'y':
                 self.sacar(self.__saldo)
-                print(f'Saque de R${self.__saldo:.2f} autorizado na conta {self.id}')
+                print(f'Saque de R${self.__saldo:.2f} autorizado na conta {self._id}')
         else:
             self.__saldo -=valor
-            print(f'Saque de R${valor:.2f} autorizado na conta {self.id}')
+            print(f'Saque de R${valor:.2f} autorizado na conta {self._id}')
