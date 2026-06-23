1b6742193a5eda065f27190ee7cfb69eb2cf8369
ViniNovack <vininomn@gmail.com>
2026-06-22 22:24:37 -0300

    Termino do desafio 32

--- aula 12/Desafio 32/ContaBancaria.py
+++ aula 12/Desafio 32/ContaBancaria.py
@@ -1,7 +1,7 @@
 import hashlib
 
 class ContaBancaria:
-    def __init__(self, id, nome, senha='', saldo=0):
+    def __init__(self, id, nome, saldo=0, senha:str=''):
         self._id = id
         self._titular = nome
         self.__saldo = saldo
@@ -9,9 +9,9 @@ class ContaBancaria:
             senha = str(input("Digite uma senha para sua protação: "))
         self.__senha = hashlib.sha256(senha.encode('utf-8')).hexdigest()
 
-    def __str__(self, senha=''):
+    def __str__(self, senha:str=''):
         if senha == '':
-            senha = str(input("Digite uma senha para sua protação: "))
+            senha = str(input("Digite uma senha para sua proteção: "))
             senha = hashlib.sha256(senha.encode('utf-8')).hexdigest()
         else:
             senha = hashlib.sha256(senha.encode('utf-8')).hexdigest()
@@ -20,7 +20,7 @@ class ContaBancaria:
             return f'Acesso CONCEDIDO, Bem vindo {self._titular}\nEstado atual da conta: {self.__dict__}'
         else:
             return 'Acesso NEGADO!'
-
+#   ------------------------------------- SALDO ---------------------------------
     @property
     def saldo(self):
         return self.__saldo
@@ -31,19 +31,19 @@ class ContaBancaria:
             print('Não é permitido trocar o valor da conta, apenas fazer saques e depositos')
         else:
             print('Não é permitido trocar o valor da conta, apenas fazer saques e depositos')
-
+#   ------------------------------------- TITULAR ---------------------------------
     @property
-    def titular(self):
+    def nome(self):
         return self._titular
     
-    @titular.setter
-    def saldo(self, nome):
+    @nome.setter
+    def nome(self, nome):
         if isinstance(nome, int):
             print('Um nome não pode ter apenas números\nNome novo INVALIDO')
         else:
             self._titular = nome
             print(f'O nome {nome} foi registrado com sucesso!')
-
+#   ------------------------------------- ID ---------------------------------
     @property
     def id(self):
         return self._id
@@ -54,19 +54,58 @@ class ContaBancaria:
             self._id = id
         else:
             print('A troca foi invalida, só se podem números no id')
-
-    def depositar(self, valor):
-        valor = abs(valor)
-        self.__saldo +=valor
-        print(f'Depósito de R${valor:.2f} autorizado na conta {self._id}')
-
-    def sacar(self, valor):
-        valor = abs(valor)
-        if valor > self.__saldo:
-            x = str(input('Você não tem todo esse dinheiro, mas você pode sacar todo o valor da comta? (Y/N)\n'))
-            if (x.lower()).strip() == 'y':
-                self.sacar(self.__saldo)
-                print(f'Saque de R${self.__saldo:.2f} autorizado na conta {self._id}')
+#   -------------------------------- SENHA ---------------------------------------
+    @property
+    def senha(self):
+        return self.__senha
+    
+    @senha.setter
+    def senha(self, senha:str=''):
+        if senha == '':
+            senha = str(input('Digite sua senha para sua proteção: '))
+            senha = hashlib.sha256(senha.encode('utf-8')).hexdigest()
         else:
-            self.__saldo -=valor
-            print(f'Saque de R${valor:.2f} autorizado na conta {self._id}')
+            senha = hashlib.sha256(senha.encode('utf-8')).hexdigest()
+
+        if senha == self.__senha:
+            print('Você está altorizado a trocar a sua senha!')
+            senha = str(input('Digite a nova senha: '))
+            senha = hashlib.sha256(senha.encode('utf-8')).hexdigest()
+            self.__senha = senha
+        else:
+            print('Senha errada, troca de senha INVALIDA!')
+#   ------------------------------------------------------------------------------
+    
+    def depositar(self, valor, senha:str=''):
+        if senha == '':
+            senha = str(input('Digite sua senha: '))
+            senha = hashlib.sha256(senha.encode('utf-8')).hexdigest()
+        else:
+            senha = hashlib.sha256(senha.encode('utf-8')).hexdigest()
+        
+        if senha == self.senha:
+            valor = abs(valor)
+            self.__saldo +=valor
+            print(f'Depósito de R${valor:.2f} autorizado na conta {self._id}')
+        else:
+            print('Senha está invalida, transação CANCELADA!')
+
+    def sacar(self, valor, senha:str=''):
+        if senha == '':
+            senha = str(input('Digite sua senha, para sua segurança: '))
+            senha = hashlib.sha256(senha.encode('utf-8')).hexdigest()
+        else:
+            senha = hashlib.sha256(senha.encode('utf-8')).hexdigest()
+        
+        if senha == self.senha:
+            valor = abs(valor)
+            if valor > self.__saldo:
+                x = str(input('Você não tem todo esse dinheiro, mas você pode sacar todo o valor da comta? (Y/N)\n'))
+                if (x.lower()).strip() == 'y':
+                    self.sacar(self.__saldo)
+                    print(f'Saque de R${self.__saldo:.2f} autorizado na conta {self._id}')
+            else:
+                self.__saldo -=valor
+                print(f'Saque de R${valor:.2f} autorizado na conta {self._id}')
+        else:
+            print('Senha incoreta, tranferencia INVALIDA!')
--- aula 12/Desafio 32/__main__.py
+++ aula 12/Desafio 32/__main__.py
@@ -1,7 +1,19 @@
 from ContaBancaria import ContaBancaria
+from rich import inspect
 
 def main():
-    pass
+    print('Criando a conta...')
+    cc = ContaBancaria(299792458, 'Gustavo', 1000, 'senha123')
 
+    print('Realizando depósito')
+    cc.depositar(500, 'senha123')
+
+    print('Realizando saque')
+    cc.sacar(200, 'senha123')
+
+    cc.nome = 'Vinícius'
+
+    inspect(cc, private=True, methods=True)
+    
 if __name__ == '__main__':
     main()
