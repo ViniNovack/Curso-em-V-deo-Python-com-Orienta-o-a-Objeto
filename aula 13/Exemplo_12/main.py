6a401f723a925738a679f22ff0cd5daf4f518791
ViniNovack <vininomn@gmail.com>
2026-08-03 22:31:36 -0300

    Atualização da aula 13

---
+++ aula 13/Exemplo_12/main.py
@@ -0,0 +1,19 @@
+from classes import *
+
+def main():
+    a = Cachorro("Bandit")
+    b = Gato("Mouse")
+    c = Pato("Donald")
+    d = Galinha("Pintadinha")
+    e = Spitz("Luluzinha")
+    f = PitBull("Guerreiro")
+
+    a.emitir_som()
+    b.emitir_som()
+    c.emitir_som()
+    d.emitir_som()
+    e.emitir_som()
+    f.emitir_som()
+
+if __name__ == "__main__":
+    main()
