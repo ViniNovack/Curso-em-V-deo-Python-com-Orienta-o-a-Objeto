2cb318826a926d8aa97fc88e39e73395b7eb38e6
ViniNovack <vininomn@gmail.com>
2026-08-04 20:34:11 -0300

    Exemplo 13 da Aula 13

--- aula 13/Exemplo_12/classes.py
+++ aula 13/Exemplo_12/classes.py
@@ -9,9 +9,11 @@ class Animal(ABC):
     def emitir_som(self):
         print(f'{self.nome} é {self.__class__.__name__} e está emitindo um som')
 
+
 class Pato(Animal):
     def emitir_som(self):
         print(f"{self.nome} acabou de dizer QUACK! QUACK! QUACK!")
+
 
 class Cachorro(Animal):
     def emitir_som(self):
@@ -25,9 +27,11 @@ class PitBull(Cachorro):
     def emitir_som(self):
         print(f"{self.nome} acabou de dizer RUF! RUF! RUF!")
 
+
 class Gato(Animal):
     def emitir_som(self):
         print(f"{self.nome} acabou de dizer MIAU! MIAU! MIAU!")
+
 
 class Galinha(Animal):
     def emitir_som(self):
---
+++ aula 13/Exemplo_13/main.py
@@ -0,0 +1,26 @@
+from classes import *
+
+def main():
+    p1 = Mae("Jaciara")
+    p2 = Filho("Matheus")
+    p3 = Filha("Monica")
+
+    print()
+
+    p1.fazer_pudim()
+    p1.fritar_coxinha()
+
+    print()
+
+    p2.fazer_pudim()
+    p2.fritar_coxinha()
+
+    print()
+
+    p3.fazer_pudim()
+    p3.fritar_coxinha()
+
+    print()
+
+if __name__ == "__main__":
+    main()
