ac3803d3ba3cb4fe6d0d7435619dfa2e0d18db1b
ViniNovack <vininomn@gmail.com>
2026-08-20 17:50:12 -0300

    Inicio do exercicio 38

---
+++ aula 15/Desafio_38/main.py
@@ -0,0 +1,8 @@
+from mercado.carrinho import *
+from mercado.produto import *
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
---
+++ aula 15/Desafio_38/mercado/carrinho.py
@@ -0,0 +1,5 @@
+from mercado.produto import *
+
+class Carrinho():
+    def __init__(self):
+        self.produtos = []
